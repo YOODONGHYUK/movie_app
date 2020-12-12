b3da809c6241b47e6662c07b211aa2ad77eb6031
YOODONGHYUK <ydh8485@gmail.com>
2020-12-12 21:11:21 +0900

    fill component

--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}) {
-return <h4>{title}</h4>
+function Movie({year, title, summary, poster, genres}) {
+    return (
+        <div className="movie">
+            {/* alt,title = when cursor on src, show movie name */}
+            <img src={poster} alt={title} title={title} />
+            
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {/* map function give two argument, one is current item another is item number */}
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">{genre}</li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
+            </div>
+        </div>
+    );
 }
 
 // find props that we need in api
 Movie.propTypes = {
     id: PropTypes.number.isRequired,
-    year: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
